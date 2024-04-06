2a03b499566d50e517d39b5ec10239662a8b4d53
ayan <ayan.d3v@gmail.com>
2024-04-06 17:50:01 +0500

    touches: tap, pinch, swipe, long press

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import {Touches} from "./touches";
+
 const myCanvas = document.getElementById('myCanvas')
 myCanvas.width = window.innerWidth
 myCanvas.height = window.innerHeight
 
 console.log('HELLO')
+const touchHandler = new Touches(myCanvas)
